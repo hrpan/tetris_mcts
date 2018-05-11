38b83338f71e08e4e89b66c985a68d51190b62af
Rick <panruru1224@hotmail.com>
2018-05-11 12:12:27 +0800

    Add ValueSim.py

---
+++ agents/ValueSim.py
@@ -0,0 +1,41 @@
+
+from agents.agent import Agent
+from agents.core import select_index, backup_trace, choose_action
+
+n_actions = 6
+
+class ValueSim(Agent):
+
+    def __init__(self,conf,sims,tau=None,backend='pytorch'):
+
+        super().__init__(sims=sims,backend=backend)
+
+    def mcts(self,root_index):
+        trace = select_index(root_index,self.arrs['child'],self.arrs['node_stats'])
+
+        leaf_index = trace[-1]
+        #leaf_index = select_index(root_index,self.arrs['child'],self.arrs['child_stats'])
+
+        leaf_game = self.game_arr[leaf_index]
+
+        value = leaf_game.getScore() #- self.game_arr[root_index].getScore()
+
+
+        if not leaf_game.end:
+
+            v, p = self.evaluate_state(leaf_game.getState())
+
+            value += v
+
+            for i in range(n_actions):
+                _g = leaf_game.clone()
+                _g.play(i)
+                _n = self.new_node(_g)
+
+                self.arrs['child'][leaf_index][i] = _n
+                self.arrs['node_stats'][_n][2] = _g.getScore()
+            
+        
+        backup_trace(trace,self.arrs['node_stats'],value)
+
+
