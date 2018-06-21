11db9b6df2afa45f49bcffbc01204a5d7ab7c5ea
Rick <panruru1224@hotmail.com>
2018-06-21 15:24:12 +0800

    Add ScoreTracker, dynamic agent loader

--- play.py
+++ play.py
@@ -3,47 +3,62 @@ import os
 import sys
 sys.path.append('../../pyTetris')
 from pyTetris import Tetris
-#from agent_mcts import Agent
-#from agents.FullSim import FullSim as Agent
-#from agents.Vanilla import Vanilla as Agent
-from agents.ValueSim import ValueSim as Agent
 import numpy as np
 import argparse
 import random
 import pandas as pd
 
+class ScoreTracker:
+    def __init__(self):
+        self.scores = []
+
+    def append(self, score):
+        self.scores.append(score)
+
+    def getStats(self):
+        _min = np.amin(self.scores)
+        _max = np.amax(self.scores)
+        _mean = np.mean(self.scores)
+        _std = np.std(self.scores)
+        return _min, _max, _mean, _std
+
+    def reset(self):
+        self.scores = []
+
 """
 ARGUMENTS
 """
 parser = argparse.ArgumentParser()
-parser.add_argument('--interactive',default=False,help='Text interactive interface',action='store_true')
-parser.add_argument('--selfplay',default=False,help='Agent selfplay',action='store_true')
-parser.add_argument('--app',default=1,type=int,help='Actions-per-drop')
-parser.add_argument('--cycle',default=-1,type=int,help='Number of cycle')
-parser.add_argument('--gamma',default=0.99,type=float,help='Discount factor')
-parser.add_argument('--ngames',default=50,type=int,help='Number of episodes to play')
-parser.add_argument('--mcts_sims',default=500,type=int,help='Number of MCTS sims')
-parser.add_argument('--mcts_const',default=5.0,type=float,help='PUCT constant')
-parser.add_argument('--mcts_tau',default=1.0,type=float,help='Temperature constant')
-parser.add_argument('--printboard',default=False,help='Print board',action='store_true')
-parser.add_argument('--save',default=False,help='Save self-play episodes',action='store_true')
-parser.add_argument('--save_dir',default='./data/',type=str,help='Directory for save')
-parser.add_argument('--save_file',default='data',type=str,help='Filename to save')
+parser.add_argument('--agent_type', default='Vanilla', type=str, help='Which agent to use')
+parser.add_argument('--app', default=1, type=int, help='Actions-per-drop')
+parser.add_argument('--cycle', default=-1, type=int, help='Number of cycle')
+parser.add_argument('--gamma', default=0.9, type=float, help='Discount factor')
+parser.add_argument('--interactive', default=False, help='Text interactive interface', action='store_true')
+parser.add_argument('--mcts_const', default=5.0, type=float, help='PUCT constant')
+parser.add_argument('--mcts_sims', default=500, type=int, help='Number of MCTS sims')
+parser.add_argument('--mcts_tau', default=1.0, type=float, help='Temperature constant')
+parser.add_argument('--ngames', default=50, type=int, help='Number of episodes to play')
+parser.add_argument('--printboard', default=False, help='Print board', action='store_true')
+parser.add_argument('--save', default=False, help='Save self-play episodes', action='store_true')
+parser.add_argument('--save_dir', default='./data/',type=str, help='Directory for save')
+parser.add_argument('--save_file', default='data', type=str, help='Filename to save')
+parser.add_argument('--selfplay', default=False, help='Agent selfplay', action='store_true')
 args = parser.parse_args()
 
-interactive = args.interactive
-selfplay = args.selfplay
+agent_type = args.agent_type
 app = args.app
 cycle = args.cycle
 gamma = args.gamma
-ngames = args.ngames
+interactive = args.interactive
 mcts_sims = args.mcts_sims
 mcts_const = args.mcts_const
 mcts_tau = args.mcts_tau
+ngames = args.ngames
 printboard = args.printboard
 save = args.save
 save_dir = args.save_dir
 save_file = args.save_file
+selfplay = args.selfplay
 """
 SOME INITS
 """
@@ -51,6 +66,8 @@ SOME INITS
 game = Tetris(boardsize=(22,10),actions_per_drop=app)
 
 if selfplay:
+    _agent_module = __import__('agents.'+agent_type, globals(), locals(), [agent_type], 0)
+    Agent = getattr(_agent_module,agent_type)
     agent = Agent(mcts_const,mcts_sims,tau=mcts_tau)
     agent.set_root(game)
 
@@ -58,7 +75,7 @@ if save:
     df = pd.DataFrame(columns=['board','policy','action','score','cum_score','child_stats'])
     df_ep = pd.DataFrame(columns=['board','policy','action','score','child_stats'])
 
-cum_score = 0
+tracker = ScoreTracker()
 n_games = ngames
 """
 MAIN GAME LOOP
@@ -95,14 +112,14 @@ while True:
             else:
                 break
         else:
-            cum_score += game.getScore()
+            tracker.append(game.getScore())
             n_games -= 1
             if save:
                 score = game.getScore()
                 df_ep['cum_score'] = score - df_ep['score']
                 df = pd.concat([df,df_ep],ignore_index=True)
                 df_ep = df_ep[0:0]
-            sys.stdout.write('\rGames remaining:%d Average score:%.3f'%(n_games,cum_score/(ngames-n_games)))
+            sys.stdout.write('\rGames remaining:%d min/max/mean/std:%.3f/%.3f/%.3f/%.3f'%(n_games, *tracker.getStats()))
             sys.stdout.flush()
             if n_games == 0:
                 break
@@ -116,7 +133,7 @@ sys.stdout.flush()
 
 if save:
     df['cycle']=cycle
-    _save = save_dir + save_file
+    _save = save_dir + save_file + str(cycle)
     if os.path.isfile(_save):
         df_old = pd.read_pickle(_save)
         df = pd.concat([df_old,df],ignore_index=True)
