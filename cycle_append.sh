95c82f583a9521c7edd4e4eabd1a696e246b02a4
Rick <panruru1224@hotmail.com>
2018-11-05 13:51:22 +0800

    Retab, add continual training

--- cycle_append.sh
+++ cycle_append.sh
@@ -2,41 +2,47 @@
 
 trap 'pkill -P $$' EXIT
 
-while getopts ":c" opt;do
-	case $opt in
-		c)
-			echo "Clearing previous data"
-			rm logs/*
-			rm -r data/*
-			rm -r saved_model
-			;;
-		\?)
-			echo "INVALID OPTION: -$OPTARG"	 
-			;;
-	esac
+n_worker=1
+ngames=500
+n_sims=400
+n_sims_bench=1500
+curr_cycle=1
+agent_type=ValueSim
+while getopts ":cr" opt;do
+    case $opt in
+        c)
+            echo "Clearing previous data"
+            rm logs/*
+            rm -r data/*
+            rm -r saved_model
+            rm -r pytorch_model
+            ;;
+        r)
+            echo "Returning to previous cycles"
+            last_cycle="$(ls data/self1/ | tr -d 'data' | sort -n | tail -n 1)" 
+            curr_cycle=$((last_cycle+1))
+            ;;
+        \?)
+            echo "INVALID OPTION: -$OPTARG"  
+            ;;
+    esac
 done
-
-n_worker=4
-
-ngames=1
-
-n_sims=250
 
 DATA_PATHS=""
 for ((i=1; i<=${n_worker}; i++)){
-	mkdir -p data/self${i}
-	DATA_PATHS+=" data/self${i}" 
+    mkdir -p data/self${i}
+    DATA_PATHS+=" data/self${i}" 
 }
+mkdir -p data/benchmark
 
+for ((x=$curr_cycle; x<200; x++)){
+    echo Cycle $x 
+    python train.py --sarsa --save_loss --batch_size 32 --max_iters 200000 --epochs 3 --data_dir $DATA_PATHS --val_split 0.01 --last_ncycles $n_worker --val_total 50 --save_interval 250 >> logs/log_train
 
-for ((x=1; x<200; x++)){
-	echo Cycle $x 
-	python3 train.py --new --save_loss --batch_size 32 --max_iters 200000 --epochs 25 --data_dir $DATA_PATHS --shuffle --val_split 0.01 >> logs/log_train
-
-	for ((i=1; i<=$n_worker; i++)){
-		python3 play.py --cycle $x --selfplay --ngames $ngames --mcts_sims $n_sims --save --save_dir data/self$i/ >> logs/log_$i &
-	}
-	
-	wait
+    for ((i=1; i<=$n_worker; i++)){
+        python play.py --agent_type $agent_type --cycle $x --selfplay --ngames $ngames --mcts_sims $n_sims --save --save_dir data/self$i/ >> logs/log_$i 2>> logs/log_err &
+    }
+    python play.py --agent_type $agent_type --cycle $x --selfplay --ngames 1 --mcts_sims $n_sims_bench --save --save_dir data/benchmark/ >> logs/log_benchmark 2>> logs/log_err
+    wait
 }
 
