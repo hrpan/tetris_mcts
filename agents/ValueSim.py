c1c51a1112ba1cb96761eec0c9e8f1df4e4b7ec8
Rick <panruru1224@hotmail.com>
2018-11-05 13:42:29 +0800

    Minor change to variables

--- agents/ValueSim.py
+++ agents/ValueSim.py
@@ -1,7 +1,7 @@
-
+import collections
+import numpy as np
 from agents.agent import Agent
 from agents.core import select_index, backup_trace, choose_action
-
 n_actions = 6
 
 class ValueSim(Agent):
@@ -11,7 +11,11 @@ class ValueSim(Agent):
         super().__init__(sims=sims,backend=backend)
 
     def mcts(self,root_index):
-        trace = select_index(root_index,self.arrs['child'],self.arrs['node_stats'])
+
+        _child = self.arrs['child']
+        _node_stats = self.arrs['node_stats']
+
+        trace = select_index(root_index, _child, _node_stats)
 
         leaf_index = trace[-1]
         #leaf_index = select_index(root_index,self.arrs['child'],self.arrs['child_stats'])
@@ -32,10 +36,26 @@ class ValueSim(Agent):
                 _g.play(i)
                 _n = self.new_node(_g)
 
-                self.arrs['child'][leaf_index][i] = _n
-                self.arrs['node_stats'][_n][2] = _g.getScore()
+                _child[leaf_index][i] = _n
+                _node_stats[_n][2] = _g.getScore()
             
-        
-        backup_trace(trace,self.arrs['node_stats'],value)
+        backup_trace(trace, _node_stats, value)
 
+    def compute_stats(self):
+        _stats = np.zeros((6, n_actions))
 
+        _childs = self.arrs['child'][self.root]
+        _ns = self.arrs['node_stats']
+
+        counter = collections.Counter(_childs)        
+
+        _stats[2] = 0
+        for i in range(n_actions):
+            _idx = _childs[i]
+            _stats[0][i] = _ns[_idx][0] / counter[_idx]
+            _stats[1][i] = _ns[_idx][1] / counter[_idx]
+            _stats[3][i] = _ns[_idx][1] / _ns[_idx][0]
+            _stats[4][i] = _ns[_idx][3]
+            _stats[5][i] = _ns[_idx][4]
+
+        return _stats
