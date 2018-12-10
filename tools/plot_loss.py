5700165833ecfe65eb379e71029b2a77581ec9cd
Rick <panruru1224@hotmail.com>
2018-12-10 12:12:31 +0800

    Switch to pyTables

--- tools/plot_loss.py
+++ tools/plot_loss.py
@@ -1,16 +1,14 @@
-import pandas as pd
 import matplotlib.pyplot as plt
+import tables
 import sys
 
 file_path = sys.argv[1]
+_file = tables.open_file(file_path, mode='r')
 
-loss = pd.read_pickle(file_path)
-
-l_plot = loss['loss'].plot(legend=True)
-l_v_plot = loss['loss_value'].plot(legend=True)
-l_p_plot = loss['loss_policy'].plot(legend=True)
-l_val_plot = loss['loss_val'].plot(legend=True)
-l_val_v_plot = loss['loss_val_value'].plot(legend=True)
-l_val_p_plot = loss['loss_val_policy'].plot(legend=True)
-
+table = _file.root.Loss
+for k in table.coldescrs.keys():
+    if 'loss' in k:
+        a, = plt.plot(table.col(k))
+        a.set_label(k)
+plt.legend()
 plt.show()
