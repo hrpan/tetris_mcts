c77347dfcc8154aebad0e109ddb75021d46df0fe
Rick <panruru1224@hotmail.com>
2018-12-17 13:48:36 +0800

    Use jit script from pytorch 1.0, add variance to the loss

--- model/model_pytorch.py
+++ model/model_pytorch.py
@@ -14,7 +14,7 @@ EXP_PATH = './pytorch_model/'
 def convOutShape(shape_in,kernel_size,stride):
     return ((shape_in[0] - kernel_size) // stride + 1, (shape_in[1] - kernel_size) // stride + 1 )
 
-class Net(nn.Module):
+class Net(torch.jit.ScriptModule):
 
     def __init__(self):
         super(Net,self).__init__()
@@ -32,20 +32,25 @@ class Net(nn.Module):
         self.flat_in = _shape[0] * _shape[1] * filters
         self.flat_out = 64
         self.fc1 = nn.Linear(self.flat_in, self.flat_out)
-         
+        #self.flat_out = self.flat_in
+ 
         self.fc_p = nn.Linear(self.flat_out, 6)
         self.fc_v = nn.Linear(self.flat_out, 1)
+        self.fc_var = nn.Linear(self.flat_out, 1)
 
+
+    @torch.jit.script_method
     def forward(self, x):
         x = self.bn1(F.relu(self.conv1(x)))
         x = self.bn2(F.relu(self.conv2(x)))
-        x = x.view(-1,self.flat_in)
+        x = x.view(-1, 1728)
+
         x = F.relu(self.fc1(x))
 
         policy = F.softmax(self.fc_p(x), dim=1)
         value = torch.exp(self.fc_v(x))
-
-        return value, policy
+        var = torch.exp(self.fc_var(x))
+        return value, var, policy
 
 def convert(x):
     return torch.from_numpy(x.astype(np.float32))
@@ -54,7 +59,6 @@ class Model:
     def __init__(self,new=True):
 
         self.model = Net()
-        
         self.optimizer = optim.Adam(self.model.parameters())
         self.scheduler = None
 
@@ -62,17 +66,19 @@ class Model:
 
         state = convert(batch[0])
         value = convert(batch[1])
+        variance = convert(batch[2])
         policy = convert(batch[3])
 
-        _v, _p = self.model(state)
+        _v, _var, _p = self.model(state)
 
         loss_v = F.mse_loss(_v, value)
+        loss_var = F.mse_loss(_var, variance)
         #loss_v = Variable(torch.FloatTensor([0]))
         #loss_p = F.kl_div(torch.log(_p),policy)
         loss_p = Variable(torch.FloatTensor([0]))
 
-        loss = loss_v + loss_p
-        return loss, loss_v, loss_p
+        loss = loss_v + loss_var + loss_p
+        return loss, loss_v, loss_var, loss_p
 
     def compute_loss(self,batch):
 
