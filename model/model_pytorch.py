629eb3fdc1923f386fa465a203f34e9a345a6979
Rick <panruru1224@hotmail.com>
2019-11-02 18:24:54 +0800

    Add reset_optimizer, revamp loss calculations

--- model/model_pytorch.py
+++ model/model_pytorch.py
@@ -5,9 +5,10 @@ import torch.nn.functional as F
 import torch.nn.utils as U
 import torch.onnx
 import torch.utils.data as D
-import onnx
+#import onnx
 import os, sys, subprocess
 import numpy as np
+from collections import defaultdict
 from caffe2.python import workspace
 
 IMG_H, IMG_W, IMG_C = (22,10,1)
@@ -39,13 +40,13 @@ class Net(nn.Module):
         #self.conv1 = nn.Conv2d(1, filters, kernel_size, stride)
         self.conv1 = nn.Conv2d(1, filters, kernel_size=4, stride=(2, 1))
         self.bn1 = nn.BatchNorm2d(filters)
-        #self.gn1 = nn.GroupNorm(4, filters, affine=False)
+        self.gn1 = nn.GroupNorm(4, filters, affine=False)
         #_shape = convOutShape((22,10), kernel_size, stride)
         _shape = convOutShape((22,10), 4, stride=(2, 1))
 
         self.conv2 = nn.Conv2d(filters, filters, kernel_size, stride)
         self.bn2 = nn.BatchNorm2d(filters)
-        #self.gn2 = nn.GroupNorm(4, filters, affine=False)
+        self.gn2 = nn.GroupNorm(4, filters, affine=False)
         _shape = convOutShape(_shape, kernel_size, stride)
 
         flat_in = _shape[0] * _shape[1] * filters
@@ -59,13 +60,13 @@ class Net(nn.Module):
         self.fc_v = nn.Linear(flat_out, 1)
         self.fc_var = nn.Linear(flat_out, 1)
         torch.nn.init.normal_(self.fc_v.bias, mean=1000, std=.1)
-        torch.nn.init.normal_(self.fc_var.bias, mean=1000, std=.1)
+        torch.nn.init.normal_(self.fc_var.bias, mean=1e4, std=.1)
 
     def forward(self, x):
-        x = self.bn1(F.relu(self.conv1(x)))
-        x = self.bn2(F.relu(self.conv2(x)))
-        #x = self.gn1(F.relu(self.conv1(x)))
-        #x = self.gn2(F.relu(self.conv2(x)))
+        #x = self.bn1(F.relu(self.conv1(x)))
+        #x = self.bn2(F.relu(self.conv2(x)))
+        x = self.gn1(F.relu(self.conv1(x)))
+        x = self.gn2(F.relu(self.conv2(x)))
         #x = F.relu(self.conv1(x))
         #x = F.relu(self.conv2(x))
         x = x.view(x.shape[0], -1)
@@ -76,8 +77,8 @@ class Net(nn.Module):
         policy = torch.ones((x.shape[0], 7)) / 7
         #value = torch.exp(self.fc_v(x))
         value = self.fc_v(x)
-        var = torch.exp(self.fc_var(x)) + 1
-        #var = F.softplus(self.fc_var(x)) + 1
+        #var = torch.exp(self.fc_var(x)) + 1
+        var = F.softplus(self.fc_var(x)) + 1 
         return value, var, policy
 
 def convert(x):
@@ -96,7 +97,7 @@ class Dataset(D.Dataset):
 
 
 class Model:
-    def __init__(self, training=False, new=True, weighted_mse=False, ewc=False, ewc_lambda=1, mle_gauss=True, use_variance=True, use_policy=False, loss_type='mae', use_onnx=False, use_cuda=True):
+    def __init__(self, training=False, new=True, weighted_mse=False, ewc=False, ewc_lambda=1, use_variance=True, use_policy=False, loss_type='kldiv', use_onnx=False, use_cuda=True):
 
         self.use_cuda = use_cuda
 
@@ -112,10 +113,9 @@ class Model:
         if self.use_cuda:
             self.model = self.model.cuda()        
         self.optimizer = optim.Adam(self.model.parameters(), lr=1e-3, eps=1e-1)
-        #self.optimizer = optim.AdamW(self.model.parameters(), lr=1e-4, eps=1e-5, weight_decay=1e0)
-        #self.optimizer = optim.SGD(self.model.parameters(), lr=1e-3, momentum=0.9)
-        #self.scheduler = None
-        self.scheduler = optim.lr_scheduler.LambdaLR(self.optimizer, lambda epoch: max(0.5 ** epoch, 1e-2))
+        #self.optimizer = optim.SGD(self.model.parameters(), lr=1e-3, momentum=0.95, nesterov=True)
+        self.scheduler = None
+        #self.scheduler = optim.lr_scheduler.LambdaLR(self.optimizer, lambda epoch: max(0.5 ** epoch, 1e-2))
 
         self.v_mean = 0
         self.v_std = 1
@@ -128,8 +128,6 @@ class Model:
         self.ewc = ewc
         self.ewc_lambda = ewc_lambda
 
-        self.mle_gauss = mle_gauss
-
         self.fisher = None
 
         self.use_policy = use_policy
@@ -137,8 +135,12 @@ class Model:
 
         if loss_type == 'mae':
             self.l_func = lambda x, y: torch.abs(x-y)
-        else:
+        elif loss_type == 'mse':
             self.l_func = lambda x, y: (x - y) ** 2
+        elif loss_type == 'mle':
+            self.l_func = lambda v_p, mu_p, v, mu: torch.log(v_p / v) + v / v_p - 1 + (mu - mu_p) ** 2 / v_p
+        elif loss_type == 'kldiv':
+            self.l_func = lambda v_p, mu_p, v, mu: 0.5 * torch.log(v_p / v) + 0.5 * (v_p + (mu - mu_p) ** 2) / v - 0.5
 
         self.use_onnx = use_onnx
 
@@ -160,22 +162,26 @@ class Model:
             weight = weight.cuda()
 
         _v, _var, _p = self.model(state)
-        if self.mle_gauss:
-            loss = (weight * (torch.log(_var) + variance / _var + ((value - _v) ** 2 ) / _var - 1 - torch.log(variance))).mean()
+        if loss_type == 'mle':
+            loss = (weight * self.l_func(_var, _v, variance, value)).mean()
+            return loss, torch.FloatTensor([0]), torch.FloatTensor([0]), torch.FloatTensor([0])
+        elif loss_type == 'kldiv':
+            loss = self.l_func(_var, _v, variance, value).mean()
             return loss, torch.FloatTensor([0]), torch.FloatTensor([0]), torch.FloatTensor([0])
         else:
-            if self.weighted_mse:
-                loss_v = (weight * self.l_func(_v, value)).mean()
-                if self.use_variance:
-                    loss_var = (weight * self.l_func(_var, variance)).mean()
-                else:
-                    loss_var = torch.FloatTensor([0])
+            loss_v = self.l_func(_v, value)
+
+            if self.use_variance:
+                loss_var = self.l_func(_var, variance)
             else:
-                loss_v = self.l_func(_v, value).mean()
-                if self.use_variance:
-                    loss_var = self.l_func(_var, variance).mean()
-                else:
-                    loss_var = torch.FloatTensor([0])
+                loss_var = torch.FloatTensor([0])
+
+            if self.weighted:
+                loss_v = weight * loss_v
+                loss_var = weight * loss_var
+
+            loss_v = loss_v.mean()
+            loss_var = loss_var.mean()
 
             if self.use_policy:
                 loss_p = F.kl_div(torch.log(_p),policy)
@@ -183,6 +189,7 @@ class Model:
                 loss_p = torch.FloatTensor([0])
 
             loss = loss_v + loss_var + loss_p
+
             return loss, loss_v, loss_var, loss_p
 
     def compute_ewc_loss(self):
@@ -233,7 +240,7 @@ class Model:
         #    norm += p.grad.data.norm(2).item() ** 2
         #norm = norm ** 0.5
         #print(' ', norm)
-        #U.clip_grad_norm_(self.model.parameters(), 100)
+        #U.clip_grad_norm_(self.model.parameters(), 10)
       
         self.optimizer.step()
 
@@ -353,6 +360,9 @@ class Model:
         result[0] = v
         return result
 
+    def reset_optimizer(self):
+
+        self.optimizer.state = defaultdict(dict)
 
     def update_scheduler(self, **kwarg):
 
