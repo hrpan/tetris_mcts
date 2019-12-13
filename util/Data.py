0312f92878bdd43b595786ee7ad5d08a3ef05406
Rick <panruru1224@hotmail.com>
2019-12-14 02:14:07 +0800

    Cleanup

--- util/Data.py
+++ util/Data.py
@@ -3,11 +3,13 @@ import numpy as np
 
 n_actions = 7
 
+
 def keyFile(s):
     for i, c in enumerate(s):
         if c.isdigit() and s[i-4:i] == 'data':
             break
     return int(s[i:])
+
 
 class State(tables.IsDescription):
     episode        = tables.Int32Col()
@@ -18,15 +20,16 @@ class State(tables.IsDescription):
     lines          = tables.Int32Col()
     score          = tables.Int32Col()
     child_stats    = tables.Float32Col(shape=(6, n_actions))
-    cycle          = tables.Int32Col() 
+    cycle          = tables.Int32Col()
     value          = tables.Float32Col()
     variance       = tables.Float32Col()
+
 
 class Loss(tables.IsDescription):
     loss_train               = tables.Float32Col()
     loss_train_value         = tables.Float32Col()
     loss_train_variance      = tables.Float32Col()
-    loss_train_policy        = tables.Float32Col() 
+    loss_train_policy        = tables.Float32Col()
     loss_validation          = tables.Float32Col()
     loss_validation_value    = tables.Float32Col()
     loss_validation_variance = tables.Float32Col()
@@ -34,18 +37,19 @@ class Loss(tables.IsDescription):
     loss_ewc                 = tables.Float32Col()
     cycle                    = tables.Int32Col()
 
+
 class DataSaver:
 
     def __init__(self, save_dir, save_file, cycle, chunksize=500):
 
-        file_name = save_dir + save_file + str(cycle) 
+        file_name = save_dir + save_file + str(cycle)
 
         self.chunksize = chunksize
-        
+
         self.cycle = cycle
-        
+
         self.file = tables.open_file(file_name, mode='a')
-    
+
         self.iter = 0
 
         if self.file.__contains__('/State'):
@@ -56,8 +60,8 @@ class DataSaver:
         self.state = self.table.row
 
     def add(self, episode, action, agent, game):
-        
-        self.iter += 1 
+
+        self.iter += 1
         self.state['episode'] = episode
         self.state['board'] = game.getState()
         self.state['policy'] = agent.get_prob()
@@ -65,7 +69,7 @@ class DataSaver:
         self.state['combo'] = game.getCombo()
         self.state['lines'] = game.getLines()
         self.state['score'] = game.getScore()
-        self.state['child_stats'] = agent.get_stats()         
+        self.state['child_stats'] = agent.get_stats()
         self.state['cycle'] = self.cycle
         v, var = agent.get_value()
         self.state['value'] = v
@@ -78,7 +82,10 @@ class DataSaver:
         if self.iter == 0:
             self.table.flush()
 
-    def add_raw(self, episode, board, policy, action, combo, lines, score, child_stats, value, variance):
+    def add_raw(self, episode, board,
+                policy, action, combo,
+                lines, score, child_stats,
+                value, variance):
 
         self.iter += 1
         self.state['episode'] = episode
@@ -101,9 +108,9 @@ class DataSaver:
             self.table.flush()
 
     def close(self):
-        
+
         self.table.flush()
-    
+
         self.file.close()
 
     def save_episode(self):
@@ -112,15 +119,15 @@ class DataSaver:
 
 
 class DataLoader:
-    
+
     def __init__(self, list_of_files):
 
         list_of_files = sorted(list_of_files, key=keyFile)
- 
+
         self.files = [tables.open_file(f, mode='r') for f in list_of_files]
 
         self.tables = [f.root.State for f in self.files]
-    
+
         self.episode = np.concatenate([t.col('episode') for t in self.tables])
         self.board = np.concatenate([t.col('board') for t in self.tables])
         self.policy = np.concatenate([t.col('policy') for t in self.tables])
@@ -132,7 +139,7 @@ class DataLoader:
         self.cycle = np.concatenate([t.col('cycle') for t in self.tables])
         self.value = np.concatenate([t.col('value') for t in self.tables])
         self.variance = np.concatenate([t.col('variance') for t in self.tables])
-        
+
         self.length = len(self.episode)
 
         for f in self.files:
@@ -145,7 +152,7 @@ class DataLoader:
             return 0
         else:
             return index
-    
+
     def getBoard(self, index):
         index = self.bound_index(index)
         return self.board[index]
@@ -178,7 +185,7 @@ class LossSaver:
         self.cycle = cycle
 
         self.file = tables.open_file('data/loss', mode='a')
-        
+
         if self.file.__contains__('/Loss'):
             self.table = self.file.root.Loss
         else:
@@ -206,7 +213,7 @@ class LossSaver:
 
         self.table.flush()
 
-    def add_raw(self, losses):
+    def add_raw(self, l):
 
         self.iter += 1
 
@@ -222,14 +229,11 @@ class LossSaver:
         self.loss['cycle'] = self.cycle
 
         self.loss.append()
-                
+
         self.iter = self.iter % self.chunksize
         if self.iter == 0:
             self.table.flush()
 
     def close(self):
-        self.table.flush() 
+        self.table.flush()
         self.file.close()
-
-
-
