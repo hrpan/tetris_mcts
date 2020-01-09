946807dc90a83b9be73720157e81802e80439db2
Rick <panruru1224@hotmail.com>
2020-01-10 00:27:07 +0800

    Random agent

---
+++ agents/Random.py
@@ -0,0 +1,15 @@
+from random import randint
+
+class Random:
+
+    def __init__(self, **kwargs):
+        pass
+
+    def play(self):
+        return randint(0, 6)
+
+    def update_root(self, game, episode):
+        pass
+
+    def close(self):
+        pass
