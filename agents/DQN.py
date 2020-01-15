d7c8dbd5f96470c6e553a5b551d097360f905c15
Rick <panruru1224@hotmail.com>
2020-01-15 17:10:59 +0800

    +double +distributional

--- agents/DQN.py
+++ agents/DQN.py
@@ -4,14 +4,16 @@ import torch.nn as nn
 import torch.optim as optim
 import torch.nn.functional as F
 from random import random, randint
+from copy import deepcopy
+from math import floor
 
 
 class Model(nn.Module):
     def __init__(self):
         super(Model, self).__init__()
-        self.conv1 = nn.Conv2d(1, 16, kernel_size=3)
-        self.conv2 = nn.Conv2d(16, 16, kernel_size=3)
-        self.fc1 = nn.Linear(1728, 128)
+        self.conv1 = nn.Conv2d(1, 32, kernel_size=3)
+        self.conv2 = nn.Conv2d(32, 32, kernel_size=3)
+        self.fc1 = nn.Linear(3456, 128)
         self.fc_v = nn.Linear(128, 7)
 
     def forward(self, x):
@@ -24,15 +26,57 @@ class Model(nn.Module):
         return v
 
 
+class DistModel(nn.Module):
+    def __init__(self, atoms=50):
+        super(DistModel, self).__init__()
+        self.conv1 = nn.Conv2d(1, 32, kernel_size=3)
+        self.conv2 = nn.Conv2d(32, 32, kernel_size=3)
+        self.fc1 = nn.Linear(3456, 128)
+        self.fc_v = nn.Linear(128, 7 * atoms)
+        self.atoms = atoms
+
+    def forward(self, x):
+        x = F.relu(self.conv1(x))
+        x = F.relu(self.conv2(x))
+        x = x.view(x.shape[0], -1)
+        x = F.relu(self.fc1(x))
+        x = self.fc_v(x).view(x.shape[0], 7, self.atoms)
+        p = F.softmax(x, dim=2)
+
+        return p
+
+
+def prob_to_exp(p, atoms, vmin, vmax):
+    bin_center = (torch.arange(atoms) + 0.5) * (vmax - vmin) / atoms
+    q = (p * bin_center).sum(dim=2)
+    return q
+
+
 class DQN:
 
-    def __init__(self, threads=2, memory_size=1000000, gamma=0.99, batch_size=32, **kwargs):
+    def __init__(self,
+                 threads=2,
+                 memory_size=1000000,
+                 gamma=0.99,
+                 eps_init=1,
+                 eps_final=0.01,
+                 batch_size=32,
+                 target=True,
+                 target_update_steps=100,
+                 distributional=True,
+                 distributional_atoms=50,
+                 distributional_vmax=10000,
+                 distributional_vmin=0,
+                 **kwargs):
 
         torch.set_num_threads(threads)
 
         self.batch_size = batch_size
 
-        self.eps = 1
+        self.eps_init = eps_init
+        self.eps_final = eps_final
+
+        self.eps = eps_init
 
         self.gamma = gamma
 
@@ -48,15 +92,28 @@ class DQN:
             np.empty((memory_size, ), dtype=np.bool)
         ]
 
-        self.model = Model()
+        self.distributional = distributional
+        self.distributional_atoms = distributional_atoms
+        self.distributional_vmax = distributional_vmax
+        self.distributional_vmin = distributional_vmin
+
+        if distributional:
+            self.model = DistModel(atoms=distributional_atoms)
+        else:
+            self.model = Model()
+
+        self.target = target
+        self.target_update_steps = target_update_steps
+        self.target_steps = 0
+
+        if target:
+            self.target_model = deepcopy(self.model)
 
         self.last_score = 0
 
         self.fresh_start = True
 
         self.optimizer = optim.Adam(self.model.parameters(), lr=1e-4, eps=1e-3)
-
-        self.loss = nn.SmoothL1Loss()
 
     def play(self):
 
@@ -66,7 +123,14 @@ class DQN:
             action = randint(0, 6)
         else:
             with torch.no_grad():
-                q_value = self.model(state_tensor)[0]
+                if self.distributional:
+                    q_dist = self.model(state_tensor)
+                    atoms = self.distributional_atoms
+                    vmin = self.distributional_vmin
+                    vmax = self.distributional_vmax
+                    q_value = prob_to_exp(q_dist, atoms, vmin, vmax)[0]
+                else:
+                    q_value = self.model(state_tensor)[0]
             action = q_value.argmax().item()
 
         self.last_action = action
@@ -80,16 +144,49 @@ class DQN:
         si, a, r, sf, end = [x[idx] for x in self.memory]
         qi = self.model(torch.from_numpy(si))
 
-        qf = self.model(torch.from_numpy(sf)).max(1)[0]
+        if self.target:
+            qf = self.target_model(torch.from_numpy(sf))
+            self.target_steps = (self.target_steps + 1) % self.target_update_steps
+            if self.target_steps == 0:
+                self.save_model()
+                self.target_model = deepcopy(self.model)
 
-        y = qi.clone().detach()
-        for i in range(self.batch_size):
-            if end[i]:
-                y[i, a[i]] = torch.tensor(r[i])
-            else:
-                y[i, a[i]] = r[i] + self.gamma * qf[i]
+        else:
+            qf = self.model(torch.from_numpy(sf))
 
-        loss = self.loss(qi, y)
+        if self.distributional:
+            gamma = self.gamma
+            atoms = self.distributional_atoms
+            vmin = self.distributional_vmin
+            vmax = self.distributional_vmax
+            delta = (vmax - vmin) / atoms
+            qf_exp = prob_to_exp(qf, atoms, vmin, vmax)
+            qf_max_action = qf_exp.argmax(dim=1)
+            y = torch.zeros(qi.shape)
+            loss = torch.tensor(0)
+            for i in range(self.batch_size):
+                for atom in range(atoms):
+                    lb = r[i] + gamma * (atom * delta + vmin)
+                    ub = lb + delta * gamma
+                    lb_bin = (lb - vmin) / delta
+                    ub_bin = (ub - vmin) / delta
+
+                    _p = qf[i, qf_max_action[i], atom]
+                    fraction = (floor(ub_bin) - lb_bin) / gamma
+                    y[i, a[i], int(floor(lb_bin))] += _p * fraction
+                    y[i, a[i], int(floor(ub_bin))] += _p * (1 - fraction)
+            loss = - (y * torch.log(qi)).sum() / self.batch_size
+        else:
+            qf = qf.max(1)[0]
+            y = qi.clone().detach()
+
+            for i in range(self.batch_size):
+                if end[i]:
+                    y[i, a[i]] = torch.tensor(r[i])
+                else:
+                    y[i, a[i]] = r[i] + self.gamma * qf[i]
+
+            loss = F.smooth_l1_loss(qi, y)
 
         self.optimizer.zero_grad()
 
@@ -97,7 +194,7 @@ class DQN:
 
         self.optimizer.step()
 
-        self.eps = 1 - 0.99 * min(self.memory_index / self.memory_size, 1)
+        self.eps = self.eps_init + (self.eps_final - self.eps_init) * min(self.memory_index / self.memory_size, 1)
 
     def update_root(self, game, episode=None):
 
@@ -133,6 +230,12 @@ class DQN:
             self.train_step()
         if self.memory_index % 1000 == 0:
             print('Memory index: {:10}    Epsilon: {:5.3f}'.format(self.memory_index, self.eps), flush=True)
+
+    def save_model(self):
+        torch.save({
+                'optimizer_state_dict': self.optimizer.state_dict(),
+                'model_state_dict': self.model.state_dict(),
+            }, './pytorch_model/dqn_ckpt')
 
     def close(self):
         pass
