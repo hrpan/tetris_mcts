10a390494116a2c743bff7b84dea7d372a8ce6f7
Rick <panruru1224@hotmail.com>
2020-04-07 20:11:26 +0800

    Use new model class

--- tools/inference_speed.py
+++ tools/inference_speed.py
@@ -1,9 +1,9 @@
-import model.model_pytorch as M
+from model.model_vv import Model_VV as Model
 import numpy as np
 from tqdm import tqdm 
 
 batch_size = 1
-m = M.Model()
+m = Model(training=False)
 b = np.ones((batch_size, 1, 22, 10))
 for i in tqdm(range(10000)):
     m.inference(b)
