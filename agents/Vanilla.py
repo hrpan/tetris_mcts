7af655c21d895c40cc8472c970303a5550539921
Rick <panruru1224@hotmail.com>
2020-05-16 15:39:14 +0800

    Adapt to new API and cpp functions

--- agents/Vanilla.py
+++ agents/Vanilla.py
@@ -1,75 +1,64 @@
+import agents.helper
 from agents.agent import TreeAgent
-from agents.core import *
-from agents.core_projection import *
-
+from agents.core import select_trace, backup_trace
 from random import randint
+from agents.cppmodule.core import backup_trace_obs, select_trace_obs
 
 
 class Vanilla(TreeAgent):
-    def __init__(self, **kwargs):
+    def __init__(self, gamma=0.99, **kwargs):
 
-        super().__init__(projection=False, **kwargs)
+        super().__init__(projection=True, **kwargs)
+
+        self.gamma = gamma
 
         self.g_tmp = self.env(*self.env_args)
 
-    def mcts(self, root_index):
+    def mcts(self, root_index, sims):
+
+        child = self.arrays['child']
+        score = self.arrays['score']
 
         if self.projection:
-            trace = select_trace_obs(
-                root_index,
-                self.arrays['child'],
-                self.obs_arrays['visit'],
-                self.obs_arrays['value'],
-                self.obs_arrays['variance'],
-                self.arrays['score'],
-                self.node_to_obs,
-                low=5)
+            visit = self.obs_arrays['visit']
+            value = self.obs_arrays['value']
+            variance = self.obs_arrays['variance']
+            n_to_o = self.node_to_obs
+            s_args = [root_index, child, visit, value, variance, score, n_to_o, 5]
+            selection = select_trace_obs
+            b_args = [None, visit, value, variance, n_to_o, score, None, None, self.gamma]
+            #backup = backup_trace_mixture_obs
+            backup = backup_trace_obs
         else:
-            trace = select_trace(
-                root_index,
-                self.arrays['child'],
-                self.arrays['visit'],
-                self.arrays['value'],
-                self.arrays['variance'],
-                self.arrays['score'],
-                low=5)
+            visit = self.arrays['visit']
+            value = self.arrays['value']
+            variance = self.arrays['variance']
+            s_args = [root_index, child, visit, value, variance, score, 5]
+            selection = select_trace
+            b_args = [None, visit, value, variance, score, None, None, self.gamma]
+            backup = backup_trace
 
-        leaf_index = trace[-1]
+        for i in range(sims):
+            trace = selection(*s_args)
+            leaf_index = trace[-1]
 
-        leaf_game = self.game_arr[leaf_index]
+            leaf_game = self.game_arr[leaf_index]
 
-        if not leaf_game.end:
+            if not leaf_game.end:
 
-            _g = self.g_tmp
-            _g.copy_from(leaf_game)
+                _g = self.g_tmp
+                _g.copy_from(leaf_game)
+                while not _g.end:
+                    _act = randint(0, self.n_actions-1)
+                    _g.play(_act)
+                _value = _g.score
+                _variance = 1e3
 
-            while not _g.end:
-                _act = randint(0, self.n_actions-1)
-                _g.play(_act)
+                self.expand(leaf_game)
+            else:
+                _value, _variance = leaf_game.score, 0
 
-            value = _g.score
-            var = 1e3
-            
-            self.expand(leaf_game)
-
-        else:
-            value = leaf_game.score
-            var = 0
-
-        if self.projection:
-            backup_trace_obs(
-                trace,
-                self.obs_arrays['visit'],
-                self.obs_arrays['value'],
-                self.obs_arrays['variance'],
-                self.node_to_obs,
-                self.arrays['score'],
-                value, var)
-        else:
-            backup_trace_welford_v2(
-                trace,
-                self.arrays['visit'],
-                self.arrays['value'],
-                self.arrays['variance'],
-                self.arrays['score'],
-                value, var)
+            b_args[0] = trace
+            b_args[-3] = _value
+            b_args[-2] = _variance
+            backup(*b_args)
