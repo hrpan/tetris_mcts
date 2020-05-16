75f3139e709e0d3f26b9ef7d1b7033bc33b3ff61
Rick <panruru1224@hotmail.com>
2020-05-16 15:37:35 +0800

    Add backup_trace_obs

--- agents/cppmodule/core.cpp
+++ agents/cppmodule/core.cpp
@@ -191,6 +191,41 @@ py::array_t<int, 1> select_trace_obs(
     return py::array_t<int, 1>(trace.size(), &trace[0]);
 }
 
+void backup_trace_obs(
+        py::array_t<int, 1> &trace,
+        py::array_t<int, 1> &visit,
+        py::array_t<float, 1> &value,
+        py::array_t<float, 1> &variance,
+        py::array_t<int, 1> &n_to_o,
+        py::array_t<float, 1> &score,
+        double _value,
+        double _variance,
+        double gamma){
+
+    auto trace_uc = trace.unchecked<1>();
+    auto visit_uc = visit.mutable_unchecked<1>();
+    auto value_uc = value.mutable_unchecked<1>();
+    auto variance_uc = variance.mutable_unchecked<1>();
+    auto score_uc = score.unchecked<1>();
+    auto n_to_o_uc = n_to_o.unchecked<1>();
+
+    for(int i=trace.size() - 1; i >= 0; --i){
+        int idx = trace_uc(i);
+        _value -= score_uc(idx);
+        int o = n_to_o_uc(idx);
+        if(visit_uc(o) == 0){
+            value_uc(o) = _value;
+            variance_uc(o) = _variance;
+        }else{
+            double delta = _value - value_uc(o);
+            value_uc(o) += delta / (visit_uc(o) + 1);
+            double delta2 = _value - value_uc(o);
+            variance_uc(o) += (delta * delta2 - variance_uc(o)) / (visit_uc(o) + 1);
+        }
+        visit_uc(o) += 1;
+        _value = gamma * _value + score_uc(idx); 
+    }
+}
 
 /*
     PYBIND11
@@ -199,4 +234,5 @@ py::array_t<int, 1> select_trace_obs(
 PYBIND11_MODULE(core, m){
     m.def("get_all_childs", &get_all_childs);
     m.def("select_trace_obs", &select_trace_obs);
+    m.def("backup_trace_obs", &backup_trace_obs);
 }
