ba955c57f2075ad590380a6d0588e5b147e1d031
Rick <panruru1224@hotmail.com>
2020-05-17 01:26:34 +0800

    Add more info printing

--- tools/test.py
+++ tools/test.py
@@ -1,21 +1,31 @@
-import model.model_pytorch as M
+#from model.model_vp import Model_VP as M
+from model.model_vv import Model_VV as M
 import torch
 import numpy as np
 
-np.set_printoptions(suppress=True, threshold=100000, precision=20)
+np.set_printoptions(suppress=True, threshold=100000, precision=3)
 
-m = M.Model(use_cuda=False)
+m = M(use_cuda=False)
 m.load()
 
+pars = 0
+for p in m.model.parameters():
+    pars += np.prod(p.size())
+
+print('Parameters: {}'.format(pars))
+
+print(m.model.value_bound.item(), m.model.variance_bound.item())
+#print(m.model.head.norm_out.bias)
+#print(m.model.head.norm_out.weight)
+print(m.model.head.fc_out.bias)
+#print('value mean/std: {:.3f}/{:.3f}'.format(m.model.value_mean.item(), m.model.value_std.item()))
+#print('variance mean/std: {:.3f}/{:.3f}'.format(m.model.variance_mean.item(), m.model.variance_std.item()))
 b = np.random.randint(0, 2, (18, 1, 22, 10))
 
 for i in range(18):
     b[i,:,0:i+4,:] = 0
 b[:,:,1,5:7] = -1
 b[:,:,2,5:7] = -1
-
-print(m.model.fc_v.bias)
-print(m.model.fc_var.bias)
 
 y = m.inference(b)
 
@@ -27,11 +37,11 @@ b[:,0,0:5,:] = 0
 b_flip = np.flip(b, axis=3).copy()
 y = m.inference(b)
 y_flip = m.inference(b_flip)
-print(np.average((y[0] - y_flip[0]) ** 2))
-print(np.average((y[1] - y_flip[1]) ** 2))
+print(np.sqrt(np.average((y[0] - y_flip[0]) ** 2)))
+print(np.sqrt(np.average((y[1] - y_flip[1]) ** 2)))
 
 print('lr', m.optimizer.param_groups[0]['lr'])
-keys = ['exp_avg_sq', 'max_exp_avg_sq', 'exp_avg_sq_last', 'momentum_buffer', 'exp_inf', 'square_avg']
+keys = ['avg', 'avg_sq', 'exp_avg', 'exp_avg_sq', 'max_exp_avg_sq', 'exp_avg_sq_last', 'momentum_buffer', 'exp_inf', 'square_avg']
 for g in m.optimizer.param_groups:
     for p in g['params']:
         
@@ -40,5 +50,6 @@ for g in m.optimizer.param_groups:
             continue
         for k in keys:
             if k in state:
-                print('{} {:10.3e} {:10.3e}'.format(k, state[k].mean().item(), state[k].std().item()), end='    ')
+                print('{} {:10.3e} {:10.3e} {:10.3e} {:10.3e}'.format(k, state[k].mean().item(), state[k].std().item(), state[k].min().item(), state[k].max().item()), end='    ')
+
         print()
