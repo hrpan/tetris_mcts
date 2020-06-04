fc27dc6830b4117e7c4c1ca9b33af4ff7df3f484
Rick <panruru1224@hotmail.com>
2020-06-05 02:41:11 +0800

    Log-y plotting

--- tools/plot_dump.py
+++ tools/plot_dump.py
@@ -17,28 +17,32 @@ v_size = int(len(state) * 0.1)
 _data_train = [d[:-v_size] for d in _data]
 _data_valid = [d[-v_size:] for d in _data]
 
-def plot_data(data, bins=100, suffix=''):
+def plot_data(data, bins=200, suffix=''):
 
     value, variance, weight = data
 
     plt.xlabel('value')
     plt.hist(value, bins=bins)
+    plt.yscale('log')
     plt.savefig('./tmp/value_hist{}.png'.format(suffix))
     plt.cla()
     plt.clf()
 
     plt.xlabel('variance')
     plt.hist(variance, bins=bins)
+    plt.yscale('log')
     plt.savefig('./tmp/variance_hist{}.png'.format(suffix))
     plt.cla()
     plt.clf()
 
     plt.xlabel('weight')
     plt.hist(weight, bins=bins)
+    plt.yscale('log')
     plt.savefig('./tmp/weight_hist{}.png'.format(suffix))
     plt.cla()
     plt.clf()
 
+    plt.yscale('linear')
     plt.xlabel('value')
     plt.ylabel('variance')
     plt.hist2d(value, variance, bins=bins, norm=mpl.colors.LogNorm())
