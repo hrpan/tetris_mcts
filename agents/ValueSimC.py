48203ffcd8fc701d242dfaa919450e30b4b8ea16
Rick <panruru1224@hotmail.com>
2020-06-16 14:08:41 +0800

    Arg styling

--- agents/ValueSimC.py
+++ agents/ValueSimC.py
@@ -9,14 +9,14 @@ def training(state, value, variance, visit, d_size, model):
     model.train_data([state[:d_size], value[:d_size], 
                       variance[:d_size], visit[:d_size]],
                      iters_per_val=100, batch_size=512, max_iters=50000,
-                     sample_replacement=False)
+                     sample_replacement=True, oversampling=False)
     model.training(False)
 
 class ValueSimC(OnlineMCTSAgent):
 
     def __init__(
-            self, sims=100, max_nodes=100000, online=False, memory_size=500000, memory_growth_rate=5000,
-            min_visit=15, projection=True, gamma=0.99, benchmark=False, leaf_parallel=True, **kwargs):
+            self, sims=100, max_nodes=100000, online=False, accumulation_policy=0, memory_size=500000, episodes_per_train=25,
+            memory_growth_rate=5000, min_visit=40, projection=True, gamma=0.999, benchmark=False, leaf_parallel=True, **kwargs):
 
         self.model = Model()
         self.model.load()
@@ -25,9 +25,21 @@ class ValueSimC(OnlineMCTSAgent):
         inference = self.model.inference
 
         super().__init__(
-            sims, max_nodes, online, memory_size, memory_growth_rate, min_visit,
-            projection, gamma, benchmark, inference, 0, 
-            lambda state, val, var, vis, size: training(state, val, var, vis, size, self.model), leaf_parallel)
+            sims=sims,
+            max_nodes=max_nodes,
+            online=online,
+            accumulation_policy=accumulation_policy,
+            memory_size=memory_size,
+            episodes_per_train=episodes_per_train,
+            memory_growth_rate=memory_growth_rate,
+            min_visit=min_visit,
+            projection=projection,
+            gamma=gamma,
+            benchmark=benchmark,
+            evaluator=inference,
+            evaluation_type=0,
+            train=lambda state, val, var, vis, size: training(state, val, var, vis, size, self.model),
+            LP=leaf_parallel)
 
     def close(self):
         pass
