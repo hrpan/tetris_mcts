0e130e60135800f6d93ba3460e50779a047224b8
Rick <panruru1224@hotmail.com>
2020-08-30 16:00:52 +0800

    Change some default parameters

--- tools/train_dump.py
+++ tools/train_dump.py
@@ -14,4 +14,4 @@ print(states.shape)
 
 _data = [states, values, variance, weights]
 
-m.train_data(_data, batch_size=512, max_iters=50000, iters_per_val=100, early_stopping_patience=10, early_stopping=True, validation_fraction=0.1)
+m.train_data(_data, batch_size=1024, max_iters=50000, iters_per_val=100, early_stopping_patience=100, early_stopping=True, validation_fraction=0.1, oversampling=False, weighted=True)
